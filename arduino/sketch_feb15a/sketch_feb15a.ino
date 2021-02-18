f4cc28f923a7db456208f7b0bd3c6dc842d89166
Jacob Teske <teske.jacob@gmail.com>
2021-02-18 08:44:02 -0600

    initial commit

--- arduino/sketch_feb15a/sketch_feb15a.ino
+++ arduino/sketch_feb15a/sketch_feb15a.ino
@@ -1,9 +1,19 @@
+int speedPin = 11;
+//int directionPin = 13;
+//int onPin = 12;
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(speedPin, OUTPUT);
+  //pinMode(onPin, OUTPUT);
+  //pinMode(directionPin, OUTPUT);
+  analogWrite(speedPin, 127);
+  //digitalWrite(directionPin, LOW);
+  //digitalWrite(onPin, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
