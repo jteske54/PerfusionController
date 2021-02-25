01a2f75b6fd372abda17f4098c8981b155a03c64
Jacob Teske <teske.jacob@gmail.com>
2021-02-25 10:31:32 -0600

    Finished Arduino Firmware

--- arduino/sketch_feb15a/sketch_feb15a.ino
+++ arduino/sketch_feb15a/sketch_feb15a.ino
@@ -1,19 +1,41 @@
 int speedPin = 11;
-//int directionPin = 13;
-//int onPin = 12;
-
+int directionPin = 13;
+int onPin = 12;
+int recOn;
+int recDir;
+int recSpeed;
+int speedSet;
+int directionSet;
+int onSet;
+int rec;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(19200);
+  Serial.setTimeout(100);
   pinMode(speedPin, OUTPUT);
-  //pinMode(onPin, OUTPUT);
-  //pinMode(directionPin, OUTPUT);
+  pinMode(onPin, OUTPUT);
+  pinMode(directionPin, OUTPUT);
+  digitalWrite(onPin, HIGH);
   analogWrite(speedPin, 127);
-  //digitalWrite(directionPin, LOW);
-  //digitalWrite(onPin, LOW);
+  digitalWrite(directionPin, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  while(Serial.available()){
+  rec = Serial.parseInt();
+  Serial.println(rec);
+  recOn = ((rec % 100)-(rec % 10))/10;
+  recDir = (rec % 10);
+  recSpeed = floor(rec/100);
+  Serial.print(recOn);
+  Serial.print(",");
+  Serial.print(recDir);
+  Serial.print(",");
+  Serial.println(recSpeed);
+  digitalWrite(onPin, recOn);
+  digitalWrite(directionPin, recDir);
+  analogWrite(speedPin, recSpeed);
+  }
 }
